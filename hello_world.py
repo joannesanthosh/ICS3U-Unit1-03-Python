c7d68e038a6b61ed50fc56cf0ffedf8adabe2014
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-09-15 15:18:00 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    # This function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()   
